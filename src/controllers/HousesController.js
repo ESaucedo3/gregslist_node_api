5ffddc6a4db1c34bdcdd1860e36e7f9320878906
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-09-10 14:48:13 -0600

    GET House/Houses complete

--- src/controllers/HousesController.js
+++ src/controllers/HousesController.js
@@ -1,7 +1,41 @@
+import { housesService } from "../services/HousesService.js";
 import BaseController from "../utils/BaseController.js";
 
 export class HousesController extends BaseController {
   constructor() {
     super("api/houses");
+    this.router
+      .get("", this.getFilteredHouses)
+      .get("", this.getHouses)
+      .get("/:houseId", this.getSpecificHouse);
+  }
+
+  async getHouses(request, response, next) {
+    try {
+      const houses = await housesService.getHouses();
+      response.send(houses);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getSpecificHouse(request, response, next) {
+    try {
+      const houseId = request.params.houseId;
+      const house = await housesService.getSpecificHouse(houseId);
+      response.send(house);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getFilteredHouses(request, response, next) {
+    try {
+      const query = request.query;
+      const houses = await housesService.getFilteredHouses(query);
+      response.send(houses);
+    } catch (e) {
+      next(e);
+    }
   }
 }
--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -1,3 +1,20 @@
-class HousesService {}
+import { dbContext } from "../db/DbContext.js";
+
+class HousesService {
+  async getFilteredHouses(query) {
+    const sortBy = query.sort;
+    delete query.sort;
+    const houses = await dbContext.Houses.find(query).sort(sortBy);
+    return houses;
+  }
+  async getSpecificHouse(houseId) {
+    const house = await dbContext.Houses.findById(houseId);
+    return house;
+  }
+  async getHouses() {
+    const houses = await dbContext.Houses.find();
+    return houses;
+  }
+}
 
 export const housesService = new HousesService();
