fa4eefb5fd722ec955811bda38d5bd0df07257ef
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-09-10 16:34:36 -0600

    quicksave2

--- src/services/PetsService.js
+++ src/services/PetsService.js
@@ -1,4 +1,5 @@
 import { dbContext } from "../db/DbContext.js";
+import { BadRequest } from "../utils/Errors.js";
 
 class PetsService {
   async getPets() {
@@ -6,12 +7,35 @@ class PetsService {
     return pets;
   }
   async getSpecificPet(petId) {
-    const pet = await dbContext.Pets.findById(petId);
+    const pet = await dbContext.Pets.findById(petId).populate("creator");
+    if (pet === null) {
+      throw new BadRequest(`No pet was found with an id of ${petId}`);
+    }
+
     return pet;
   }
   async getFilteredPets(query) {
-    const pets = await dbContext.Pets.find(query);
-    return pets;
+    const sortBy = query.sort;
+    delete query.sort;
+
+    const pageNumber = parseInt(query.page) || 1;
+    const limitAmount = 5;
+    const skipAmount = (pageNumber - 1) * limitAmount;
+    delete query.page;
+
+    const pets = await dbContext.Pets.find(query)
+      .sort(sortBy)
+      .skip(skipAmount)
+      .limit(limitAmount)
+      .populate("creator");
+
+    const petCount = await dbContext.Pets.countDocuments(query);
+    return {
+      results: pets,
+      count: petCount,
+      currentPage: pageNumber,
+      totalPages: Math.ceil(petCount / limitAmount),
+    };
   }
 }
 
